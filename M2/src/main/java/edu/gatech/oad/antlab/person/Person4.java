5e5e81949c25ebd644856401479c85ed22862b62
Emily Cahill <c.emily@rocketmail.com>
2014-01-22 15:29:25 -0500

    changed Person4

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -30,8 +30,13 @@ public class Person4 {
      * @return the modified string
      */
     private String calc(String input) {
-      //Person 1 put your implementation here
-      return null;
+      char[] s = input.toCharArray();
+      for (int i = 0; i < s.length; i ++)
+      {
+         int ans = s[i] + 1;
+         s[i] = (char)ans;
+      }
+      return new String(s);
     }
     
     /**
@@ -45,6 +50,5 @@ public class Person4 {
     public String toString(String input) {
       return name + calc(input);
     }
-
 }
 
