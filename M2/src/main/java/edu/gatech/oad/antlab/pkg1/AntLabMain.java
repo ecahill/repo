502ebf484c84e42050213d27afda2c9ae99b8aaa
jwu308 <jwu308@gatech.edu>
2014-01-23 16:57:44 -0500

    updated AntLabMain.java

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -74,7 +74,7 @@ import edu.gatech.oad.antlab.pkg2.*;
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
                   Person5 p5 = new Person5("Jesse Wu");
-                  toPrint += p5.toString("jwu308");
+                  toPrint += p5.toString("902912064");
 		  
         System.out.println(toPrint);
         
